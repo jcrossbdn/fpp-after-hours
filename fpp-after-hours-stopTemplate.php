1ca734af568911056790679f521963efe8a1289d
Jason Cross <jcrossbdn@users.noreply.github.com>
2018-12-03 15:33:14 -0600

    Delete fpp-after-hours-stopTemplate.php

--- fpp-after-hours-stopTemplate.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*autoupdated*/$pluginDirectory="/home/fpp/media/plugins/";
-/*autoupdated*/$pluginDataDirectory="/home/fpp/media/plugindata/";
-
-$vol=false;
-if (file_exists($pluginDataDirectory.'fpp-after-hours-showVolume'))  $vol=intval(file_get_contents($pluginDataDirectory.'fpp-after-hours-showVolume'));
-exec("mpc stop && mpc clear".($vol !== false ? " && mpc volume $vol" : ""));
-file_put_contents($pluginDataDirectory."fpp-after-hours-streamRunning","0");
-?>
