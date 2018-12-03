f579d79ab49f7fdb8160a0b10ce731f861bcf933
Jason Cross <jcrossbdn@users.noreply.github.com>
2018-12-03 15:33:27 -0600

    Delete nowPlaying.php

--- nowPlaying.php
+++
@@ -1,8 +0,0 @@
-<?php
-exec('mpc current',$song);
-exec("mpc",$status);
-$status=implode("<br>",$status);
-if (isset($song[0])) $status=str_replace($song[0]."<br>","",$status);
-if (isset($song[0])) echo "{$song[0]}<hr>{$status}";
-else echo "----No stream playing----";
-?>
