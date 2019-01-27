3737c3f67d23166e6889cc9dce054872bb442af8
Jason Cross <jcrossbdn@users.noreply.github.com>
2019-01-27 11:41:42 -0600

    added help file linking back to git repo

---
+++ help/fpp-after-hours.php
@@ -0,0 +1,3 @@
+<?php
+echo "<br><br><a href='https://github.com/jcrossbdn/fpp-after-hours/blob/master/README.md' target='_blank'>Please click here to go to the help page</a>";                       
+?>
