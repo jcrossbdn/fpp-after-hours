a90d27a7bbe34309e28a01f6390ca43d92544519
Jason Cross <jcrossbdn@users.noreply.github.com>
2026-09-10 23:33:02 -0500

    Issue 46

--- commands/fpp-after-hours-stop.php
+++ commands/fpp-after-hours-stop.php
@@ -18,7 +18,7 @@ if (isset($argv[1]) && isset($argv[2])) {
         if (!isset($vol)) $volStr="";
         else $volStr = " $vol";
         //file_put_contents('/home/fpp/media/plugindata/teststop',"$os - mpc volume$volStr\n",FILE_APPEND);
-        exec("mpc volume$volStr",$volRet);
+        exec("{$fah->mpcPath} volume$volStr",$volRet);
         $volRet=array_reverse($volRet);
         foreach ($volRet as $v) {
             if (substr($v,0,7)=='volume:') {
@@ -43,15 +43,15 @@ if (isset($argv[1]) && isset($argv[2])) {
         usleep($delay*1000);
     } while ($vol > 0);
     $vol=$fah->showVolume;
-    exec("mpc stop && mpc clear".($vol !== false ? " && mpc volume $vol" : ""));
+    exec("{$fah->mpcPath} stop && {$fah->mpcPath} clear".($vol !== false ? " && {$fah->mpcPath} volume $vol" : ""));
 }
 else {
-    exec("mpc stop"); //release the sound card as fast as possible
+    exec("{$fah->mpcPath} stop"); //release the sound card as fast as possible
     require_once '/home/fpp/media/plugins/fpp-after-hours/fpp-after-hours-class.php';
     $fah=new fppAfterHours();
     $fah->setMusicRunningStatus(false);
     $fah->setCurrentInternetRadioHost();
     $vol=$fah->showVolume;
-    exec("mpc stop && mpc clear".($vol !== false ? " && mpc volume $vol" : ""));
+    exec("{$fah->mpcPath} stop && {$fah->mpcPath} clear".($vol !== false ? " && {$fah->mpcPath} volume $vol" : ""));
 }
 ?>
